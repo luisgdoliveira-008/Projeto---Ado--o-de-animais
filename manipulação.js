353fe103a1ad05c3db1f9a429ab4a06a9d6c3ac9
luisgdoliveira-008-2213 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-04-07 09:27:07 -0300

    ADICIONEI MAIS VARIAVEIS NO JS

--- manipulação.js
+++ manipulação.js
@@ -5,6 +5,19 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
   let email = document.getElementById("email").value;
   let moradia = document.getElementById("moradia").value;
   let quintal = document.querySelector('input[name="quintal"]:checked');
+  let pet = document.querySelector('input[name="pet"]:checked');
+  let telefone = document.getElementById("moradia").value;
+  let cpf = document.getElementById("cpf").value;
+  let idade = document.getElementById("idade").value;
+  let cidade = document.querySelector('input[name="cidade"]:checked');
+  let horas = document.querySelector('input[name="horas"]:checked');
+  let motivo = document.querySelector('input[name="motivo"]:checked');
+  let aceitar= document.querySelector('input[name="aceitar"]:checked');
+
+
+
+
+
 
   if(nome.length < 3) return alert("Nome Inválido");
 
